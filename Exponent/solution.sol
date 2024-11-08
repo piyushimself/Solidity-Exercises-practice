370c812bd5e68e5d2f967e315996fbf81f7c33fe
Piyush Jain <piyush24jain@gmail.com>
2024-11-08 18:34:11 +0530

    Create solution.sol

---
+++ Exponent/solution.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract Exponent {
+    /*
+        This exercise assumes you know how exponent works.
+        1. Function `getResult` should return the result of the exponent.
+         Function state mutability can be restricted to pure
+    */
+
+    function getResult(
+        uint256 _base,
+        uint256 _e
+    ) public pure returns (uint256) {
+        return (_base ** _e);
+    }
+}
