054f4ad555bf056680755962a166a99735cc70eb
Piyush Jain <piyush24jain@gmail.com>
2024-11-09 13:28:07 +0530

    Create solution.sol

---
+++ ListOfNumbers/solution.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract ListOfNumbers {
+    uint256[] private arr;
+
+    /// let caller append @param number to the array "arr"
+    function appendToArray(uint256 number) public {
+        // your code here
+        arr.push(number);
+    }
+
+    /// return arr
+    function getArray() public view returns (uint256[] memory) {
+        // your code here
+        return arr;
+    }
+}
