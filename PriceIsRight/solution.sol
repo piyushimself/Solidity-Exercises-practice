313e8c67954a2e12073c79091e8868c1a6139725
Piyush Jain <piyush24jain@gmail.com>
2024-11-10 03:14:02 +0530

    Create solution.sol

---
+++ PriceIsRight/solution.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.13;
+
+contract PriceIsRight {
+    /// @dev make this function revert unless exactly 1 ether is sent as value in the function call
+    function buy() public payable {
+        /// your code here
+        require(msg.value == 1 ether, "You must send exactly 1 Ether");
+    }
+}
